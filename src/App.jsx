c2ea167ea19a6ab0f61910865128ccb0cf4373a1
Manas <manaskumar1400@gmail.com>
2026-03-26 14:18:34 +0530

    Background and Header Coded succesfully.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import FloatingLines from './component/FloatingLines';
+import Header from './component/header_section/Header';
+
 
 const App = () => {
   return (
-    <div className='bg-gray-400'>Hello  I am amanas</div>
-    
+    <div style={{ position: 'relative', minHeight: '100vh', overflow: 'hidden' }}>
+      <div style={{ position: 'fixed', inset: 0, zIndex: -1 }}>
+        <FloatingLines
+          enabledWaves={["top", "middle", "bottom"]}
+          lineCount={5}
+          lineDistance={5}
+          bendRadius={5}
+          bendStrength={-0.5}
+          interactive={true}
+          parallax={true}
+        />
+      </div>
+
+      <div style={{ position: 'relative', zIndex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Header />
+      </div>
+    </div>
   )
 }
 
